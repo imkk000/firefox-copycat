2df5b58b4464c8b72b4708fee8a6aed6c7a9db84
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2025-05-13 21:37:20 +0700

    Add request header override

--- background.js
+++ background.js
@@ -1,27 +1,56 @@
 const sortTabs = async () => {
+  const tabsCount = new Map()
+  const hostnames = new Set()
   const tabs = await browser.tabs.query({ currentWindow: true })
-  tabs
+  const sortedTabs = tabs
     .map(tab => {
-      const { id } = tab
+      const { id, groupId } = tab
       const { hostname, pathname } = new URL(tab.url)
-      return { id, hostname, pathname }
+      tabsCount[hostname] = tabsCount[hostname] || 0
+      tabsCount[hostname]++
+      hostnames.add(hostname)
+      return { id, groupId, hostname, pathname }
     })
     .sort((a, b) => {
-      if (a.hostname === b.hostname) {
-        return a.pathname < b.pathname
+      if (tabsCount[a.hostname] !== tabsCount[b.hostname]) {
+        return tabsCount[a.hostname] < tabsCount[b.hostname]
       }
-      return a.hostname > b.hostname
+      if (a.hostname !== b.hostname) {
+        return a.hostname < b.hostname
+      }
+      return a.pathname < b.pathname
     })
-    .forEach((tab, i) => {
-      browser.tabs.move(tab.id, { index: i })
-    });
+
+  sortedTabs.forEach((tab, i) => {
+    browser.tabs.move(tab.id, { index: i })
+  })
+
+  for (const hostname of hostnames) {
+    if (tabsCount[hostname] <= 5 || hostname.length === 0) {
+      continue
+    }
+    const ids = sortedTabs
+      .filter(tab => tab.hostname === hostname)
+      .map(tab => tab.id)
+    const groupId = await browser.tabs.group({ tabIds: ids })
+    await browser.tabGroups.update(groupId, { title: hostname, collapsed: false })
+  }
 }
+
+browser.browserAction.onClicked.addListener(async () => {
+  await sortTabs();
+})
+
+browser.webRequest.onBeforeSendHeaders.addListener(req => {
+  const headers = req.requestHeaders.filter(({ name }, i) => {
+    name = name.toLowerCase()
+    return name !== "user-agent" && name !== "referer" && name !== "origin"
+  })
+  headers.push({ name: "User-Agent", value: "Mozilla/5.0" })
+  return { requestHeaders: headers }
+}, { urls: ["<all_urls>"] }, ["requestHeaders"])
 
 browser.runtime.onInstalled.addListener(async () => {
   console.log("Tabs extension installed");
-});
-
-browser.browserAction.onClicked.addListener(async _ => {
-  await sortTabs();
 })
 
